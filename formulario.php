92ccdcb69f820a26db0c219576a206e35a710e52
Camilo <camilo.svs@gmail.com>
2019-06-28 00:31:40 -0400

    Revert "Carpetas"
    This reverts commit cc394a2ee1d74517475d35c410d1d395af4f8cfd.

---
+++ formulario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guardar</title>
+</head>
+<body>
+	<center>
+	
+		<form action="operacion_guardar.php" method="POST">
+			<input type="text" REQUIRED name="isbn" placeholder="isbn..." value""/>
+			<input type="text" REQUIRED name="codigotitulo" placeholder="codigo titulo..." value""/>
+			<input type="text" REQUIRED name="numpaginas" placeholder="numpaginas..." value""/>
+			<input type="text" REQUIRED name="tamaño" placeholder="tamaño..." value""/>
+			<input type="text" REQUIRED name="refedit" placeholder="ref edit..." value""/>
+		<input type="submit" value="aceptar" />
+			
+		</form>
+		
+		
+	</center>
+</body>
+</html>
