d3d121b60a61234e80061eed801e428ac6c36470
Camilo <camilo.svs@gmail.com>
2019-06-27 21:36:25 -0400

    guardarEditorial

---
+++ guardarEditorial.php
@@ -0,0 +1,21 @@
+<?php
+
+include("conexion.php");
+
+	$codigo= $_POST['codigo'];
+	$nombre= $_POST['text'];
+	$telefono= $_POST['telefono'];
+    $direccion= $_POST['direccion'];
+    
+	$insertSQL = "INSERT INTO editorial(codigo,nombre,telefono,direccion)"
+        . "values ('$codigo','$nombre','$telefono','$direccion')";
+
+    $resultado_insertSQL = pg_query($conexion,$insertSQL);
+
+	if($resultado){
+		echo "insercion exitosa";
+	}
+	else{
+		echo "insecion no exitosa";
+	}
+ ?>
