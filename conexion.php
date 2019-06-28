9014265d50e16dfa052029aa0c0009a71c0f2141
Marcelo <bobyandlobo@gmail.com>
2019-06-27 21:17:55 -0400

    lib

---
+++ conexion.php
@@ -0,0 +1,4 @@
+<?php
+	$conexion= pg_connect('host=localhost dbname=Libreria user=postgres password=slipknot123');
+
+?>
---
+++ tabla.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tabla</title>
+</head>
+<body>
+	<center>
+		<table>
+			<thead>
+				<tr>
+					<th> <a href="formulario.php">Nuevo<a/></th>
+					<th>Lista de Ediciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+				<td>isbn</td>
+				<td>codigo titulo</td>
+				<td>numero paginas</td>
+				<td>tamaño</td>
+				<td>ref editorial</td>
+				<td>modificar</td>
+				<td>eliminar</td>
+
+					
+				</tr>
+			</tbody>
+		</table>
+	</center>
+
+</body>
+</html>
